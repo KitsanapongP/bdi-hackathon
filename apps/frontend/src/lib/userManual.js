2fc25290021bcc4d2bd5be43800292fd09ee38ee
KitsanapongP <kitsanapong.p@kkumail.com>
2026-04-10 22:32:51 +0700

    fix: update hackathon criteria file path to the correct document

--- apps/frontend/src/lib/userManual.js
+++ apps/frontend/src/lib/userManual.js
@@ -1,5 +1,5 @@
 import USER_MANUAL_FILE from '../assets/documents/user-manuals/BDI-Young-Innovator-Hackathon_USERMANUAL.pdf';
-import HACKATHON_CRITERIA_FILE from '../assets/documents/about/hackathon-criteria.pdf';
+import HACKATHON_CRITERIA_FILE from '../assets/documents/about/เกณฑ์การให้คะแนน-BDI-Yong-Innovator.pdf';
 
 export const USER_MANUAL_PATH = USER_MANUAL_FILE;
 export const HACKATHON_CRITERIA_PATH = HACKATHON_CRITERIA_FILE;
